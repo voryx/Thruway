bc6cafe1ff7de77c42f5152567868cb1b80c6a24
David Dan <davidwdan@gmail.com>
2014-06-13 14:31:00 -0400

    Updates to the example server

--- Examples/SimpleWsServer.php
+++ Examples/SimpleWsServer.php
@@ -1,12 +1,12 @@
 <?php
 
-require '../vendor/autoload.php';
+require '../../../autoload.php';
 
 use AutobahnPHP\Transport\RatchetTransport;
 use AutobahnPHP\Peer\Router;
 
 $router = new Router();
 
-$transport = new RatchetTransport($router, "127.0.0.1", 8080);
+$transport = new RatchetTransport($router, "127.0.0.1", 9000);
 
 $transport->startTransport();
