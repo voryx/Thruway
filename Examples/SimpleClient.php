e280c5fb9ce49e05f55c94e409e020a7237d15cc
Matt Bonneau <matt@bonneau.net>
2014-06-15 17:40:00 -0400

    Added ratchet/pawl requirement removed unused WebsocketServer stuff

--- Examples/SimpleClient.php
+++ Examples/SimpleClient.php
@@ -4,8 +4,4 @@ require '../vendor/autoload.php';
 
 $loop = \React\EventLoop\Factory::create();
 
-$transport = new \AutobahnPHP\Transport\WebsocketClient('wss://some.example.com:8080', $loop);
 
-$transport->startTransport();
-
-$loop->run();
