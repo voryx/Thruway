3e3aea068ce61b9325c1a8958cdc27193b1885c1
Matt Bonneau <matt@bonneau.net>
2014-06-17 22:45:06 -0400

    Have examples figure out if you cloned or if you are in vendors.

--- Examples/SimpleWsServer.php
+++ Examples/SimpleWsServer.php
@@ -1,6 +1,10 @@
 <?php
 
-require __DIR__.'/../../../autoload.php';
+if (file_exists(__DIR__.'/../../../autoload.php')) {
+    require __DIR__.'/../../../autoload.php';
+} else {
+    require __DIR__.'/../vendor/autoload.php';
+}
 
 use AutobahnPHP\Transport\RatchetTransport;
 use AutobahnPHP\Peer\Router;
