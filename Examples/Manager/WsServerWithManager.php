f36d7f670d2b5b482aa5a4264a4bd6c90595ca39
Matt Bonneau <matt@bonneau.net>
2014-07-16 20:38:11 -0400

    Renamed and moved some Examples

--- Examples/Manager/WsServerWithManager.php
+++ Examples/Manager/WsServerWithManager.php
@@ -3,7 +3,7 @@
 if (file_exists(__DIR__.'/../../../autoload.php')) {
     require __DIR__.'/../../../autoload.php';
 } else {
-    require __DIR__.'/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 }
 
 use Thruway\Peer\Router;
