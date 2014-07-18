fef1de3ecf26efa402a97d7d1f20cc04353d5477
Matt Bonneau <matt@bonneau.net>
2014-07-17 22:21:36 -0400

    Added files from example at http://voryx.net/creating-a-custom-php-wamp-client-for-thruway/

---
+++ Examples/ClientClass/FreeSpaceClient.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This example is from the blog post:
+ * http://voryx.net/creating-a-custom-php-wamp-client-for-thruway/
+ */
+if (file_exists(__DIR__.'/../../../../autoload.php')) {
+    require __DIR__.'/../../../../autoload.php';
+} else {
+    require __DIR__ . '/../../vendor/autoload.php';
+}
+
+class FreeSpaceClient extends Thruway\Peer\Client {
+    public function getFreeSpace() {
+        return array(disk_free_space('/')); // use c: for you windowers
+    }
+    public function onSessionStart($session, $transport) {
+        $this->getCallee()->register($session, 'com.example.getfreespace', array($this, 'getFreeSpace'));
+    }
+}
---
+++ Examples/ClientClass/StartClient.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This example is from the blog post:
+ * http://voryx.net/creating-a-custom-php-wamp-client-for-thruway/
+ *
+ * NOTICE that the server is not the local server.
+ * This is because the example uses the demo server.
+ */
+if (file_exists(__DIR__.'/../../../../autoload.php')) {
+    require __DIR__.'/../../../../autoload.php';
+} else {
+    require __DIR__ . '/../../vendor/autoload.php';
+}
+require __DIR__ . '/FreeSpaceClient.php'; // so PHP can use our class
+ 
+$client = new FreeSpaceClient('myrealm');
+ 
+$client->addTransportProvider(new \Thruway\Transport\PawlTransportProvider('ws://demo.thruway.ws:9090/'));
+ 
+$client->start();
