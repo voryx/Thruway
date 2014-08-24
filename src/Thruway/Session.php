ff24d3e4207adc5330ec578651f28e2a65fedba1
Matt Bonneau <matt@bonneau.net>
2014-08-23 22:07:01 -0400

    Formatting and manager was not being set properly in constructor

--- src/Thruway/Session.php
+++ src/Thruway/Session.php
@@ -19,124 +19,136 @@ use Thruway\Transport\TransportInterface;
  * Class Session
  * @package Thruway
  */
-class Session extends AbstractSession {
+class Session extends AbstractSession
+{
 
-  /**
-   * @var AuthenticationDetails
-   */
-  private $authenticationDetails;
+    /**
+     * @var AuthenticationDetails
+     */
+    private $authenticationDetails;
 
 
-  /**
-   * @var int
-   */
-  private $messagesSent;
+    /**
+     * @var int
+     */
+    private $messagesSent;
 
-  /**
-   * @var \DateTime
-   */
-  private $sessionStart;
+    /**
+     * @var \DateTime
+     */
+    private $sessionStart;
 
-  /**
-   * @var ManagerInterface
-   */
-  private $manager;
+    /**
+     * @var ManagerInterface
+     */
+    private $manager;
 
-  function __construct(TransportInterface $transport, ManagerInterface $manager = NULL) {
-    $this->transport = $transport;
-    $this->state = static::STATE_PRE_HELLO;
-    $this->sessionId = static::getUniqueId();
-    $this->realm = NULL;
+    function __construct(TransportInterface $transport, ManagerInterface $manager = null)
+    {
+        $this->transport = $transport;
+        $this->state = static::STATE_PRE_HELLO;
+        $this->sessionId = static::getUniqueId();
+        $this->realm = null;
 
-    $this->messagesSent = 0;
-    $this->sessionStart = new \DateTime();
+        $this->messagesSent = 0;
+        $this->sessionStart = new \DateTime();
 
-    if ($manager === NULL) {
-      $manager = new ManagerDummy();
+        if ($manager === null) {
+            $manager = new ManagerDummy();
+        }
+
+        $this->setManager($manager);
+
+        $this->authenticationDetails = null;
     }
 
-    $this->setManager($this->manager);
-
-    $this->authenticationDetails = NULL;
-  }
-
-  public function sendMessage(Message $msg) {
-    $this->messagesSent++;
-    $this->transport->sendMessage($msg);
-  }
-
-  public function shutdown() {
-
-    $this->transport->close();
-  }
-
-  /**
-   *
-   */
-  public function onClose() {
-    if ($this->realm !== NULL) {
-      $this->realm->leave($this);
+    public function sendMessage(Message $msg)
+    {
+        $this->messagesSent++;
+        $this->transport->sendMessage($msg);
     }
-  }
 
-  /**
-   * Generate a unique id for sessions and requests
-   * @return mixed
-   */
-  static public function getUniqueId() {
-    // TODO: make this better
-    $result = sscanf(uniqid(), "%x");
+    public function shutdown()
+    {
 
-    return $result[0];
-  }
+        $this->transport->close();
+    }
+
+    /**
+     *
+     */
+    public function onClose()
+    {
+        if ($this->realm !== null) {
+            $this->realm->leave($this);
+        }
+    }
+
+    /**
+     * Generate a unique id for sessions and requests
+     * @return mixed
+     */
+    static public function getUniqueId()
+    {
+        // TODO: make this better
+        $result = sscanf(uniqid(), "%x");
+
+        return $result[0];
+    }
 
     /**
      * @param ManagerInterface $manager
      * @throws \InvalidArgumentException
      */
-  public function setManager($manager) {
-      $this->manager = $manager;
+    public function setManager($manager)
+    {
+        $this->manager = $manager;
 
-      if ($manager instanceof ManagerInterface) {
+        if ($manager instanceof ManagerInterface) {
 
-      } else {
-          if ($manager !== null) {
-              throw new \InvalidArgumentException("Manager must implement ManagerInterface");
-          }
-      }
-  }
+        } else {
+            if ($manager !== null) {
+                throw new \InvalidArgumentException("Manager must implement ManagerInterface");
+            }
+        }
+    }
 
-  /**
-   * @return ManagerInterface
-   */
-  public function getManager() {
-    return $this->manager;
-  }
+    /**
+     * @return ManagerInterface
+     */
+    public function getManager()
+    {
+        return $this->manager;
+    }
 
-  public function getMessagesSent() {
-    return $this->messagesSent;
-  }
+    public function getMessagesSent()
+    {
+        return $this->messagesSent;
+    }
 
-  /**
-   * @return \DateTime
-   */
-  public function getSessionStart() {
-    return $this->sessionStart;
-  }
+    /**
+     * @return \DateTime
+     */
+    public function getSessionStart()
+    {
+        return $this->sessionStart;
+    }
 
-  /**
-   * @param AuthenticationDetails $authenticationDetails
-   */
-  public function setAuthenticationDetails($authenticationDetails) {
-    $this->authenticationDetails = $authenticationDetails;
-  }
+    /**
+     * @param AuthenticationDetails $authenticationDetails
+     */
+    public function setAuthenticationDetails($authenticationDetails)
+    {
+        $this->authenticationDetails = $authenticationDetails;
+    }
 
-  /**
-   * @return AuthenticationDetails
-   */
-  public function getAuthenticationDetails() {
-    return $this->authenticationDetails;
-  }
+    /**
+     * @return AuthenticationDetails
+     */
+    public function getAuthenticationDetails()
+    {
+        return $this->authenticationDetails;
+    }
 
 
 }
