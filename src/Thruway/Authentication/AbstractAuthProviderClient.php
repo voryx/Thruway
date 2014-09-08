bc058facfeee4873d816a4aaa5ec8d7d5823bdf8
David Dan <davidwdan@gmail.com>
2014-09-08 16:49:44 -0400

    Added support for orphaned registrations and reformatted some stuff

--- src/Thruway/Authentication/AbstractAuthProviderClient.php
+++ src/Thruway/Authentication/AbstractAuthProviderClient.php
@@ -7,104 +7,100 @@ use React\Promise\Promise;
 use Thruway\Peer\Client;
 
 
-class AbstractAuthProviderClient extends Client {
+class AbstractAuthProviderClient extends Client
+{
 
-  protected $authRealms;
+    protected $authRealms;
 
-  function __construct(Array $authRealms, LoopInterface $loop = null) {
+    function __construct(Array $authRealms, LoopInterface $loop = null)
+    {
 
-    $this->authRealms = $authRealms;
+        $this->authRealms = $authRealms;
 
-    /*
-     * Set authorization the realm. Defaults to "thruway.auth"
-     *
-     * This realm is only used between the Authentication Provider Client and the Authentication Manager Client on the server.
-     *
-     */
-    parent::__construct('thruway.auth', $loop);
+        /*
+         * Set authorization the realm. Defaults to "thruway.auth"
+         *
+         * This realm is only used between the Authentication Provider Client and the Authentication Manager Client on the server.
+         *
+         */
+        parent::__construct('thruway.auth', $loop);
 
-  }
-
-  public function processHello(array $args) {
-
-    return array(
-      "CHALLENGE",
-      array(
-        "challenge" => new \stdClass(),
-        "challenge_method" => $this->getMethodName()
-      )
-    );
-  }
-
-  public function onSessionStart($session, $transport) {
-    $this->getCallee()->register(
-      $session,
-      "thruway.auth.{$this->getMethodName()}.onhello",
-      array($this, 'processHello')
-    )->then(
-      function () use ($session) {
-        $this->getCallee()->register(
-          $session,
-          "thruway.auth.{$this->getMethodName()}.onauthenticate",
-          array($this, 'preProcessAuthenticate')
-        )->then(
-          function () use ($session) {
-            $this->getCaller()->call(
-              $session,
-              'thruway.auth.registermethod',
-              array(
-                $this->getMethodName(),
-                array(
-                  "onhello" => "thruway.auth.{$this->getMethodName()}.onhello",
-                  "onauthenticate" => "thruway.auth.{$this->getMethodName()}.onauthenticate",
-                ),
-                $this->getAuthRealms()
-
-              )
-            )->then(
-              function ($args) {
-                  $this->manager->info(print_r($args, true));
-              }
-            );
-          }
-        );
-      }
-    );
-
-  }
-
-  public function preProcessAuthenticate(array $args) {
-
-    $signature = isset($args['signature']) ? $args['signature'] : NULL;
-    $extra = isset($args['extra']) ? $args['extra'] : NULL;
-
-    if (!$signature) {
-      return array("ERROR");
     }
 
-    return $this->processAuthenticate($signature, $extra);
+    public function processHello(array $args)
+    {
 
-  }
+        return ["CHALLENGE", ["challenge" => new \stdClass(), "challenge_method" => $this->getMethodName()]];
+    }
 
-  public function processAuthenticate($signature, $extra = NULL) {
+    public function onSessionStart($session, $transport)
+    {
+        $this->getCallee()->register(
+            $session,
+            "thruway.auth.{$this->getMethodName()}.onhello",
+            [$this, 'processHello'],
+            ["replace_orphaned_session" => "yes"]
+        )
+            ->then(function () use ($session) {
+                $this->getCallee()->register(
+                    $session,
+                    "thruway.auth.{$this->getMethodName()}.onauthenticate",
+                    [$this, 'preProcessAuthenticate'],
+                    ["replace_orphaned_session" => "yes"]
+                )
+                    ->then(function () use ($session) {
+                        $this->getCaller()->call($session,
+                            'thruway.auth.registermethod',
+                            [
+                                $this->getMethodName(),
+                                [
+                                    "onhello" => "thruway.auth.{$this->getMethodName()}.onhello",
+                                    "onauthenticate" => "thruway.auth.{$this->getMethodName()}.onauthenticate"
+                                ],
+                                $this->getAuthRealms()
+                            ]
+                        )
+                            ->then(function ($args) {
+                                $this->manager->info(print_r($args, true));
+                            });
+                    });
+            });
+    }
 
-    return array("SUCCESS");
+    public function preProcessAuthenticate(array $args)
+    {
 
-  }
+        $signature = isset($args['signature']) ? $args['signature'] : null;
+        $extra = isset($args['extra']) ? $args['extra'] : null;
 
-  /**
-   * @return array
-   */
-  public function getAuthRealms() {
-    return $this->authRealms;
-  }
+        if (!$signature) {
+            return ["ERROR"];
+        }
 
-  /**
-   * @param array $authRealms
-   */
-  public function setAuthRealms($authRealms) {
-    $this->authRealms = $authRealms;
-  }
+        return $this->processAuthenticate($signature, $extra);
 
+    }
 
+    public function processAuthenticate($signature, $extra = null)
+    {
+
+        return ["SUCCESS"];
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getAuthRealms()
+    {
+        return $this->authRealms;
+    }
+
+    /**
+     * @param array $authRealms
+     */
+    public function setAuthRealms($authRealms)
+    {
+        $this->authRealms = $authRealms;
+    }
 } 
