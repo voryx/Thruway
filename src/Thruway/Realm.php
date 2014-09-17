cc1a276d1f304377290f4064f4a26487f10021d6
David Dan <davidwdan@gmail.com>
2014-09-17 11:05:08 -0400

    Some refactoring and realm now handles GoodBye messages

--- src/Thruway/Realm.php
+++ src/Thruway/Realm.php
@@ -16,6 +16,7 @@ use Thruway\Manager\ManagerInterface;
 use Thruway\Message\AbortMessage;
 use Thruway\Message\AuthenticateMessage;
 use Thruway\Message\ErrorMessage;
+use Thruway\Message\GoodbyeMessage;
 use Thruway\Message\HelloMessage;
 use Thruway\Message\Message;
 use Thruway\Message\WelcomeMessage;
@@ -89,61 +90,94 @@ class Realm
      */
     public function onMessage(Session $session, Message $msg)
     {
-        if (!$session->isAuthenticated()) {
-            if ($msg instanceof HelloMessage) {
-                $this->manager->debug("got hello");
-                // send welcome message
-                if ($this->sessions->contains($session)) {
-                    $this->manager->error(
-                        "Connection tried to rejoin realm when it is already joined to the realm."
-                    );
-                    $session->sendMessage(ErrorMessage::createErrorMessageFromMessage($msg));
-                    // TODO should shut down session here
-                } else {
-                    $this->sessions->attach($session);
-                    $session->setRealm($this);
-                    $session->setState(Session::STATE_UP); // this should probably be after authentication
 
-                    if ($this->getAuthenticationManager() !== null) {
-                        $this->getAuthenticationManager()->onAuthenticationMessage($this, $session, $msg);
-                    } else {
-                        $session->setAuthenticated(true);
+        if ($msg instanceof GoodByeMessage):
+            $this->manager->info("Received a GoodBye, so shutting the session down");
+            $session->shutdown();
+        elseif ($session->isAuthenticated()):
+            $this->processAuthenticated($session, $msg);
+        elseif ($msg instanceof HelloMessage):
+            $this->processHello($session, $msg);
+        elseif ($msg instanceof AuthenticateMessage):
+            $this->processAuthenticate($session, $msg);
+        else:
+            $this->manager->error("Unhandled message sent to unauthenticated realm: " . $msg->getMsgCode());
+            $session->sendMessage(new AbortMessage(new \stdClass(), "wamp.error.not_authorized"));
+            $session->shutdown();
+        endif;
 
-                        $session->setAuthenticationDetails(AuthenticationDetails::createAnonymous());
+    }
 
-                        // the broker and dealer should give us this information
-                        $roles = array("broker" => new \stdClass, "dealer" => new \stdClass);
-                        $session->sendMessage(
-                            new WelcomeMessage($session->getSessionId(), array("roles" => $roles))
-                        );
-                    }
-                }
-            } else if ($msg instanceof AuthenticateMessage) {
-                if ($this->getAuthenticationManager() !== null) {
-                    $this->getAuthenticationManager()->onAuthenticationMessage($this, $session, $msg);
-                } else {
-                    // TODO: should shut down here probably
-                    $this->manager->error("Authenticate sent to realm without auth manager.");
-                }
-            } else {
-                $this->manager->error("Unhandled message sent to unauthenticated realm: " . $msg->getMsgCode());
-                $session->sendMessage(new AbortMessage(new \stdClass(), "wamp.error.not_authorized"));
-                $session->shutdown();
+
+    /**
+     * Process All Messages if the session has been authenticated
+     *
+     * @param Session $session
+     * @param Message $msg
+     */
+    private function processAuthenticated(Session $session, Message $msg)
+    {
+        $handled = false;
+        /* @var $role AbstractRole */
+        foreach ($this->roles as $role) {
+            if ($role->handlesMessage($msg)) {
+                $role->onMessage($session, $msg);
+                $handled = true;
+                break;
             }
+        }
+
+        if (!$handled) {
+            $this->manager->warning("Unhandled message sent to \"{$this->getRealmName()}\"");
+        }
+    }
+
+    /**
+     * @param Session $session
+     * @param HelloMessage $msg
+     */
+    private function processHello(Session $session, HelloMessage $msg)
+    {
+        $this->manager->debug("got hello");
+        // send welcome message
+        if ($this->sessions->contains($session)) {
+            $this->manager->error(
+                "Connection tried to rejoin realm when it is already joined to the realm."
+            );
+            $session->sendMessage(ErrorMessage::createErrorMessageFromMessage($msg));
+            // TODO should shut down session here
         } else {
-            $handled = false;
-            /* @var $role AbstractRole */
-            foreach ($this->roles as $role) {
-                if ($role->handlesMessage($msg)) {
-                    $role->onMessage($session, $msg);
-                    $handled = true;
-                    break;
-                }
-            }
+            $this->sessions->attach($session);
+            $session->setRealm($this);
+            $session->setState(Session::STATE_UP); // this should probably be after authentication
 
-            if (!$handled) {
-                $this->manager->warning("Unhandled message sent to \"{$this->getRealmName()}\": {$msg->getSerializedMessage()}");
+            if ($this->getAuthenticationManager() !== null) {
+                $this->getAuthenticationManager()->onAuthenticationMessage($this, $session, $msg);
+            } else {
+                $session->setAuthenticated(true);
+
+                $session->setAuthenticationDetails(AuthenticationDetails::createAnonymous());
+
+                // the broker and dealer should give us this information
+                $roles = array("broker" => new \stdClass, "dealer" => new \stdClass);
+                $session->sendMessage(
+                    new WelcomeMessage($session->getSessionId(), array("roles" => $roles))
+                );
             }
+        }
+    }
+
+    /**
+     * @param Session $session
+     * @param AuthenticateMessage $msg
+     */
+    private function processAuthenticate(Session $session, AuthenticateMessage $msg)
+    {
+        if ($this->getAuthenticationManager() !== null) {
+            $this->getAuthenticationManager()->onAuthenticationMessage($this, $session, $msg);
+        } else {
+            // TODO: should shut down here probably
+            $this->manager->error("Authenticate sent to realm without auth manager.");
         }
     }
 
