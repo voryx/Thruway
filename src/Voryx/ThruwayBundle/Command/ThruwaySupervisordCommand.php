5250b60efe2e18ccb5145188454e0da009087340
David Dan <davidwdan@gmail.com>
2014-10-30 20:08:45 -0400

    Refactored Supervisord command

--- src/Voryx/ThruwayBundle/Command/ThruwaySupervisordCommand.php
+++ src/Voryx/ThruwayBundle/Command/ThruwaySupervisordCommand.php
@@ -4,6 +4,7 @@
 namespace Voryx\ThruwayBundle\Command;
 
 
+use SupervisorClient\SupervisorClient;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,6 +14,7 @@ use Symfony\Component\Process\Process;
 
 /**
  * Class ThruwaySupervisordCommand
+ *
  * @package Voryx\ThruwayTestBundle\Command
  */
 class ThruwaySupervisordCommand extends ContainerAwareCommand
@@ -63,8 +65,11 @@ class ThruwaySupervisordCommand extends ContainerAwareCommand
 
         try {
 
-            $config = $this->getContainer()->getParameter('voryx_thruway');
+            $supervisorGroup = "thruway"; //@todo make this configurable
+            $config          = $this->getContainer()->getParameter('voryx_thruway');
+            $env             = $this->getContainer()->get('kernel')->getEnvironment();
 
+            //Check to see if Supervisord is already running
             if (file_exists($config['supervisor']['pidfile'])) {
                 $f    = fopen($config['supervisor']['pidfile'], 'r');
                 $line = fgets($f);
@@ -73,101 +78,29 @@ class ThruwaySupervisordCommand extends ContainerAwareCommand
                 return;
             }
 
-            //Create the log file if it doesn't already exist
-            $logfile    = $this->getContainer()->get('kernel')->getLogDir() . $config['supervisor']['logfile'];
-            $pidfile    = $config['supervisor']['pidfile'];
-            $configPath = $this->getContainer()->get('kernel')->locateResource($config['supervisor']['config']);
-
-            touch($logfile);
-
             $output->writeln("Starting supervisord...");
-
-            $process = new Process("{$config['supervisor']['executable']} -c {$configPath} --pidfile {$pidfile} --logfile {$logfile}");
-            $process->run(function ($type, $buffer) use ($output) {
-                if (Process::ERR === $type) {
-                    $output->writeln("Error while starting supervisord: {$buffer}");
-                } else {
-                    $output->writeln("Supervisord started");
-                }
-            });
-
-            $resourceMapper = $this->getContainer()->get('voryx.thruway.resource.mapper');
-            $mappings       = $resourceMapper->getAllMappings();
-            $supervisor     = $this->getContainer()->get('voryx.thruway.supervisor');
-            $phpBinary      = PHP_BINARY;
-            $env            = $this->getContainer()->get('kernel')->getEnvironment();
-
             $output->writeln("The environment is: {$env}");
 
-            try {
-                $supervisor->stopProcessGroup('thruway', false);
-            } catch (\Exception $e) {
-                $output->writeln("Looks like the thruway group is already stopped");
-            }
+            //Start the supervisor process
+            $this->startSupervisorProcess($config, $output);
 
-            $processes = $supervisor->getAllProcessInfo();
-            foreach ($processes as $process) {
-                if ($process['group'] === 'thruway') {
-                    $supervisor->removeProcessFromGroup('thruway', $process['name']);
-                }
-            }
+            //Get the supervisor
+            $supervisor = $this->getContainer()->get('voryx.thruway.supervisor');
 
-            try {
-                $supervisor->addProcessGroup('thruway');
-            } catch (\Exception $e) {
-                $output->writeln("The group thruway has already been added, which is okay");
-            }
+            //Stop the group, so we can clear out any old stuff
+            $this->stopProcessGroup($supervisor, $supervisorGroup, $output);
 
+            //Clear any processes that are part of the group
+            $this->clearProcessFromGroup($supervisor, $supervisorGroup, $output);
 
-            //Onetime workers
-            $defaultWorkers = [
-                "router"  => "thruway:router:start",
-                "manager" => "thruway:manager:start"
-            ];
+            //Add the group back to the process
+            $this->addProcessGroup($supervisor, $supervisorGroup, $output);
 
-            $onetimeWorkers = array_merge($defaultWorkers, $config['supervisor']['onetime_workers']);
+            //Add processes for "One Time" workers
+            $this->addOneTimeWorkers($supervisor, $config, $output, $env);
 
-            foreach ($onetimeWorkers as $workerName => $command) {
-                $output->writeln("Adding onetime worker: {$workerName}");
-
-                //Add and start the Router
-                $supervisor->addProgramToGroup('thruway', $workerName,
-                    [
-                        'command'      => "{$phpBinary} {$this->getContainer()->get('kernel')->getRootDir()}/console {$command} --env={$env}",
-                        'autostart'    => 'true',
-                        'autorestart'  => 'true',
-                        'startsecs'    => '0',
-                        'numprocs'     => 1,
-                        'process_name' => '%(program_name)s'
-                    ]);
-                sleep(3);
-            }
-
-
-            //Add the workers to the config, but don't start them yet.
-            foreach ($mappings as $workerName => $mapping) {
-                $output->writeln("Adding workers: {$workerName}");
-                $workerAnnotation = $resourceMapper->getWorkerAnnotation($workerName);
-
-                if ($workerAnnotation) {
-                    $numprocs = $workerAnnotation->getMaxProcesses() ?: $config['supervisor']['workers'];
-                } else {
-                    $numprocs = $config['supervisor']['workers'];
-                }
-
-                $supervisor->addProgramToGroup('thruway', $workerName,
-                    [
-                        'command'      => "{$phpBinary} {$this->getContainer()->get('kernel')->getRootDir()}/console thruway:worker:start {$workerName} %(process_num)d --env={$env}",
-                        'autostart'    => 'false',
-                        'autorestart'  => 'true',
-                        'startsecs'    => '0',
-                        'numprocs'     => $numprocs,
-                        'process_name' => '%(program_name)s_%(process_num)02d'
-
-                    ]);
-                sleep(3);
-                $supervisor->startProcess("thruway:{$workerName}_00");
-            }
+            //Add processes for regular Workers defined by annotations
+            $this->addWorkers($supervisor, $config, $output, $env);
 
             $output->writeln("Done");
 
@@ -178,6 +111,8 @@ class ThruwaySupervisordCommand extends ContainerAwareCommand
     }
 
     /**
+     * Stop Supervisord
+     *
      * @param OutputInterface $output
      */
     protected function stop(OutputInterface $output)
@@ -197,6 +132,8 @@ class ThruwaySupervisordCommand extends ContainerAwareCommand
     }
 
     /**
+     * Get the process status for Supervisord
+     *
      * @param OutputInterface $output
      */
     protected function status(OutputInterface $output)
@@ -216,6 +153,172 @@ class ThruwaySupervisordCommand extends ContainerAwareCommand
         } catch (\Exception $e) {
             $logger = $this->getContainer()->get('logger');
             $logger->addCritical("EXCEPTION:" . $e->getMessage());
+        }
+    }
+
+    /**
+     * Create a Logfile for Supervisord
+     *
+     * @param $config
+     * @return string
+     */
+    protected function createLogFile($config)
+    {
+        //Create the log file if it doesn't already exist
+        $logfile = $this->getContainer()->get('kernel')->getLogDir() . $config['supervisor']['logfile'];
+
+        touch($logfile);
+
+        return $logfile;
+    }
+
+    /**
+     * Start the actual Supervisord Process
+     *
+     * @param $config
+     * @param OutputInterface $output
+     */
+    protected function startSupervisorProcess($config, OutputInterface $output)
+    {
+        $logfile    = $this->createLogFile($config);
+        $pidfile    = $config['supervisor']['pidfile'];
+        $configPath = $this->getContainer()->get('kernel')->locateResource($config['supervisor']['config']);
+
+        $process = new Process("{$config['supervisor']['executable']} -c {$configPath} --pidfile {$pidfile} --logfile {$logfile}");
+        $process->run(function ($type, $buffer) use ($output) {
+            if (Process::ERR === $type) {
+                $output->writeln("Error while starting supervisord: {$buffer}");
+            } else {
+                $output->writeln("Supervisord started");
+            }
+        });
+    }
+
+    /**
+     * Stop the Supervisord process group
+     *
+     * @param SupervisorClient $supervisor
+     * @param $group
+     * @param OutputInterface $output
+     */
+    protected function stopProcessGroup(SupervisorClient $supervisor, $group, OutputInterface $output)
+    {
+        try {
+            $supervisor->stopProcessGroup($group, false);
+        } catch (\Exception $e) {
+            $output->writeln("Looks like the thruway {$group} is already stopped");
+        }
+    }
+
+    /**
+     * Clear all processes from a Supervisord group
+     *
+     * @param SupervisorClient $supervisor
+     * @param $group
+     */
+    protected function clearProcessFromGroup(SupervisorClient $supervisor, $group)
+    {
+        $processes = $supervisor->getAllProcessInfo();
+        foreach ($processes as $process) {
+            if ($process['group'] === $group) {
+                $supervisor->removeProcessFromGroup($group, $process['name']);
+            }
+        }
+    }
+
+    /**
+     * Add the Group to supervisord, if it does not exist
+     *
+     * @param SupervisorClient $supervisor
+     * @param $group
+     * @param OutputInterface $output
+     */
+    protected function addProcessGroup(SupervisorClient $supervisor, $group, OutputInterface $output)
+    {
+
+        try {
+            $supervisor->addProcessGroup($group);
+        } catch (\Exception $e) {
+            $output->writeln("The group thruway has already been added, which is okay");
+        }
+    }
+
+    /**
+     * Add "One Time" workers to Supervisord.  These are workers that will only ever have one instance running
+     *
+     * @param SupervisorClient $supervisor
+     * @param $config
+     * @param OutputInterface $output
+     * @param $env
+     */
+    protected function addOneTimeWorkers(SupervisorClient $supervisor, $config, OutputInterface $output, $env)
+    {
+
+        $phpBinary = PHP_BINARY;
+
+        //Onetime workers
+        $defaultWorkers = [
+            "router"  => "thruway:router:start",
+            "manager" => "thruway:manager:start"
+        ];
+
+        $onetimeWorkers = array_merge($defaultWorkers, $config['supervisor']['onetime_workers']);
+
+        foreach ($onetimeWorkers as $workerName => $command) {
+            $output->writeln("Adding onetime worker: {$workerName}");
+
+            //Add and start the Router
+            $supervisor->addProgramToGroup('thruway', $workerName,
+                [
+                    'command'      => "{$phpBinary} {$this->getContainer()->get('kernel')->getRootDir()}/console {$command} --env={$env}",
+                    'autostart'    => 'true',
+                    'autorestart'  => 'true',
+                    'startsecs'    => '0',
+                    'numprocs'     => 1,
+                    'process_name' => '%(program_name)s'
+                ]);
+            sleep(3);
+        }
+    }
+
+    /**
+     * Add regular workers to Supervisord.  Theses are workers that can have multiple instances running
+     *
+     * @param SupervisorClient $supervisor
+     * @param $config
+     * @param OutputInterface $output
+     * @param $env
+     */
+    protected function addWorkers(SupervisorClient $supervisor, $config, OutputInterface $output, $env)
+    {
+        $phpBinary      = PHP_BINARY;
+        $resourceMapper = $this->getContainer()->get('voryx.thruway.resource.mapper');
+        $mappings       = $resourceMapper->getAllMappings();
+
+        foreach ($mappings as $workerName => $mapping) {
+            $output->writeln("Adding workers: {$workerName}");
+            $workerAnnotation = $resourceMapper->getWorkerAnnotation($workerName);
+
+            if ($workerAnnotation) {
+                $numprocs = $workerAnnotation->getMaxProcesses() ?: $config['supervisor']['workers'];
+            } else {
+                $numprocs = $config['supervisor']['workers'];
+            }
+            //Add the workers to the config, but don't start them yet.
+            $supervisor->addProgramToGroup('thruway', $workerName,
+                [
+                    'command'      => "{$phpBinary} {$this->getContainer()->get('kernel')->getRootDir()}/console thruway:worker:start {$workerName} %(process_num)d --env={$env}",
+                    'autostart'    => 'false',
+                    'autorestart'  => 'true',
+                    'startsecs'    => '0',
+                    'numprocs'     => $numprocs,
+                    'process_name' => '%(program_name)s_%(process_num)02d'
+
+                ]);
+            sleep(3);
+
+            //Start the worker
+            $supervisor->startProcess("thruway:{$workerName}_00");
         }
     }
 }
