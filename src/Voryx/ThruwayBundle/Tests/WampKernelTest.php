4d0e621f32897dc76b3eec6518b200a61462b713
David Dan <davidwdan@gmail.com>
2015-05-20 21:05:31 -0400

    Setup proper testing

---
+++ src/Voryx/ThruwayBundle/Tests/WampKernelTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Voryx\ThruwayBundle\Tests;
+
+
+class WampKernelTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+
+}
