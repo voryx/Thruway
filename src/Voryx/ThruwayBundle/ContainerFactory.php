fe25d7c30c795e2c0167574f8fb6957708547b52
David Dan <davidwdan@gmail.com>
2015-06-25 15:28:44 -0400

    Pass the loop to the inner container

--- src/Voryx/ThruwayBundle/ContainerFactory.php
+++ src/Voryx/ThruwayBundle/ContainerFactory.php
@@ -4,18 +4,22 @@
 namespace Voryx\ThruwayBundle;
 
 
+use React\EventLoop\LoopInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Voryx\ThruwayBundle\Client\ClientManager;
 
 class ContainerFactory
 {
 
-    public static function createContainer($containerName, ClientManager $thruwayClient)
+    public static function createContainer($containerName, ClientManager $thruwayClient, LoopInterface $loop)
     {
 
         /** @var ContainerInterface $container */
         $container = new $containerName();
+
+        //These services will be passed from the outer container into the inner container
         $container->set('thruway.client', $thruwayClient);
+        $container->set('voryx.thruway.loop', $loop);
 
         return $container;
     }
