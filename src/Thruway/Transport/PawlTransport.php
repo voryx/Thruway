0364d710da95e19f3f29425aa7b14712ea4db464
Matt Bonneau <matt@bonneau.net>
2016-05-25 00:23:46 -0400

    Update Pawl to version 0.2

--- src/Thruway/Transport/PawlTransport.php
+++ src/Thruway/Transport/PawlTransport.php
@@ -2,8 +2,6 @@
 
 namespace Thruway\Transport;
 
-
-use Ratchet\WebSocket\Version\RFC6455\Frame;
 use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
 use React\Promise\Deferred;
@@ -82,56 +80,7 @@ class PawlTransport extends AbstractTransport
      */
     public function ping($timeout = 10)
     {
-        if ($timeout <= 0) {
-            return false;
-        }
-
-        $payload = $this->pingSeq;
-        $seq     = $this->pingSeq;
-
-        $this->conn->send(new Frame($payload, true, Frame::OP_PING));
-        $this->pingSeq++;
-
-        $timer = $this->loop->addTimer($timeout, function () use ($seq) {
-            if (isset($this->pingRequests[$seq])) {
-                $this->pingRequests[$seq]['deferred']->reject('timeout');
-                unset($this->pingRequests[$seq]);
-            }
-
-        });
-
-        $deferred = new Deferred();
-
-        $this->pingRequests[$seq] = [
-            'seq'      => $seq,
-            'deferred' => $deferred,
-            'timer'    => $timer
-        ];
-
-        return $deferred->promise();
-
+        return false;
+        
     }
-
-    /**
-     * Handle on pong
-     *
-     * @param \Ratchet\WebSocket\Version\RFC6455\Frame $frame
-     */
-    public function onPong(Frame $frame)
-    {
-        $seq = $frame->getPayload();
-
-        if (isset($this->pingRequests[$seq]) && isset($this->pingRequests[$seq]['deferred'])) {
-            $this->pingRequests[$seq]['deferred']->resolve();
-            /** @var TimerInterface $timer */
-            $timer = $this->pingRequests[$seq]['timer'];
-            $timer->cancel();
-
-            unset($this->pingRequests[$seq]);
-        }
-
-        // all sequence numbers before this one are probably no good anymore
-        // and actually are probably errors
-    }
-
-} 
+}
