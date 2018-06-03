eb65340f87b8f0cb05e9ee7ced6f5672ffdfdfc4
Piers Hobson <piers@thetechnologyteam.net>
2018-06-03 13:39:34 +0100

    Changed require statement to allow execution from project root, as other examples and add missing url:port to PawlTransportProvider

--- Examples/Authentication/StartClient.php
+++ Examples/Authentication/StartClient.php
@@ -1,12 +1,14 @@
 <?php
 
-require "../bootstrap.php";
+require __DIR__ . "/../bootstrap.php";
 require 'SimpleClientAuth.php';
+
+$url = "ws://127.0.0.1:9090/";
 
 $client = new \Thruway\Peer\Client('somerealm');
 
 $client->addClientAuthenticator(new SimpleClientAuth());
 
-$client->addTransportProvider(new \Thruway\Transport\PawlTransportProvider());
+$client->addTransportProvider(new \Thruway\Transport\PawlTransportProvider($url));
 
 $client->start();
